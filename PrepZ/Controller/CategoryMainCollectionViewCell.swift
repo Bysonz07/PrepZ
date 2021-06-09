df144d6f2e6acbfebb493e8f5bf311772dd05f9f
Shiddiq <syuhadashiddiq97@gmail.com>
2021-06-09 09:34:27 +0700

    start category layout

---
+++ PrepZ/Controller/CategoryMainCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CategoryMainCollectionViewCell.swift
+//  PrepZ
+//
+//  Created by Shiddiq Syuhada on 08/06/21.
+//
+
+import UIKit
+
+class CategoryMainCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
