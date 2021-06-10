b1d1aaca45d70f00fd5eba17ac61a213535acd82
Jenny C <jennycallistajc@gmail.com>
2021-06-10 12:37:05 +0700

    Homepage coba masuikin

--- PrepZ/Controller/HomePageViewController.swift
+++ PrepZ/Controller/HomePageViewController.swift
@@ -7,13 +7,25 @@
 
 import UIKit
 
-class HomePageViewController: UIViewController {
+class HomePageViewController: UIViewController,UICollectionViewDelegate, UICollectionViewDataSource{
 
+   
+    var RVImageArr = [UIImage(named: "Ayam"),UIImage(named: "Buah"),UIImage(named: "Ikan"),UIImage(named: "Sayur")]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return RVImageArr.count
+    }
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "RVImageCollectionViewCell", for: indexPath) as! RVImageCollectionViewCell
+        
+        cell.RVImg.image = RVImageArr[indexPath.row]
+        return cell
+    }
 
 }
 
